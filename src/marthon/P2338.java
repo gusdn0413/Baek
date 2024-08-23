70b5569a611cf7714d1cb9661fb81de5360911bc
gusdn0413 <gusdn0413@naver.com>
2024-08-23 12:11:32 +0900

    P2338

--- src/marthon/P2338.java
+++ src/marthon/P2338.java
@@ -1,2 +1,15 @@
-package marthon;public class P2338 {
+package marthon;
+
+import java.io.*;
+import java.math.BigInteger;
+
+public class P2338 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BigInteger a = new BigInteger(br.readLine());
+        BigInteger b = new BigInteger(br.readLine());
+        System.out.println(a.add(b));
+        System.out.println(a.subtract(b));
+        System.out.print(a.multiply(b));
+    }
 }
